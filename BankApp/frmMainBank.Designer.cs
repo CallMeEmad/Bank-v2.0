2828b9b66640dc23cb5a5e2659ffc3b35c2b309d
CallMeEmad <EmadKarimi2212@gmail.com>
2020-08-17 04:56:36 +0430

    comit

---
+++ BankApp/frmMainBank.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace BankApp
+{
+    partial class frmMainBank
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgCustomers = new System.Windows.Forms.DataGridView();
+            this.txtCostomerNum = new System.Windows.Forms.TextBox();
+            this.cmbAccessModifiers = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgCustomers
+            // 
+            this.dgCustomers.AllowUserToAddRows = false;
+            this.dgCustomers.AllowUserToDeleteRows = false;
+            this.dgCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgCustomers.Location = new System.Drawing.Point(12, 130);
+            this.dgCustomers.Name = "dgCustomers";
+            this.dgCustomers.ReadOnly = true;
+            this.dgCustomers.Size = new System.Drawing.Size(632, 224);
+            this.dgCustomers.TabIndex = 0;
+            // 
+            // txtCostomerNum
+            // 
+            this.txtCostomerNum.Location = new System.Drawing.Point(290, 32);
+            this.txtCostomerNum.Name = "txtCostomerNum";
+            this.txtCostomerNum.Size = new System.Drawing.Size(128, 20);
+            this.txtCostomerNum.TabIndex = 1;
+            this.txtCostomerNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCostomerNum_KeyPress);
+            // 
+            // cmbAccessModifiers
+            // 
+            this.cmbAccessModifiers.FormattingEnabled = true;
+            this.cmbAccessModifiers.Items.AddRange(new object[] {
+            "1 ",
+            "2",
+            "3"});
+            this.cmbAccessModifiers.Location = new System.Drawing.Point(170, 55);
+            this.cmbAccessModifiers.Name = "cmbAccessModifiers";
+            this.cmbAccessModifiers.Size = new System.Drawing.Size(248, 21);
+            this.cmbAccessModifiers.TabIndex = 2;
+            this.cmbAccessModifiers.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.cmbAccessModifiers_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(424, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "شماره مشتری : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(424, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "سطح دسترسی : ";
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(290, 82);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 5;
+            this.btnInsert.Text = "افزودن";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 360);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "محاسبه ";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmMainBank
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 395);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbAccessModifiers);
+            this.Controls.Add(this.txtCostomerNum);
+            this.Controls.Add(this.dgCustomers);
+            this.MaximumSize = new System.Drawing.Size(672, 434);
+            this.MinimumSize = new System.Drawing.Size(672, 434);
+            this.Name = "frmMainBank";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "فرم مشتریان";
+            this.Load += new System.EventHandler(this.frmMainBank_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgCustomers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgCustomers;
+        private System.Windows.Forms.TextBox txtCostomerNum;
+        private System.Windows.Forms.ComboBox cmbAccessModifiers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ BankApp/frmMainBank.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BankApp
+{
+    public partial class frmMainBank : Form
+    {
+        Dictionary<int, string> AccessModifiers = new Dictionary<int, string>();
+
+        Dictionary<long, int> Customers = new Dictionary<long, int>();
+
+        public frmMainBank()
+        {
+            InitializeComponent();
+
+            AccessModifiers.Add(1, "حق برداشت به تنهایی");
+            AccessModifiers.Add(2, "حق امضا");
+            AccessModifiers.Add(3, "حق برداشت در صورت کفایت امضا");
+
+        }
+        void LoadCustomerType()
+        {
+            cmbAccessModifiers.DataSource = AccessModifiers.ToList();
+            cmbAccessModifiers.DisplayMember = "Value";
+            cmbAccessModifiers.ValueMember = "Key";
+        }
+        private void frmMainBank_Load(object sender, EventArgs e)
+        {
+            LoadCustomerType();
+            GridSetup();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (txtCostomerNum.Text.Trim().Length < 3
+                || txtCostomerNum.Text.Trim().Length > 10)
+            {
+                MessageBox.Show("طول شناسه مشتری باید بین 3 تا 10 رقم باشد", "طول غیر مجاز");
+                return;
+            }
+
+            bool isExists = Customers.ContainsKey(Convert.ToInt32(txtCostomerNum.Text));
+            if (isExists)
+            {
+                MessageBox.Show("این شناسه قبلا استفاده شده است", "شناسه تکراری");
+                return;
+            }
+
+            Customers.Add(
+                Convert.ToInt32(txtCostomerNum.Text),
+                Convert.ToInt32(cmbAccessModifiers.SelectedValue)
+                );
+
+            dgCustomers.Rows.Add(dgCustomers.Rows.Count + 1, txtCostomerNum.Text, cmbAccessModifiers.Text);
+            RefreshForm();
+
+        }
+
+
+        private void GridSetup()
+        {
+            dgCustomers.Columns.Add("RowNum", "ردیف");
+            dgCustomers.Columns.Add("Customer", "شماره مشتری");
+            dgCustomers.Columns.Add("CustomerType", "سطح دسترسی");
+        }
+        private void cmbAccessModifiers_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void txtCostomerNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar));
+        }
+        private void RefreshForm()
+        {
+            txtCostomerNum.Text = string.Empty;
+            cmbAccessModifiers.SelectedIndex = 0;
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var mainCustomers
+                = Customers.Where(ex => ex.Value == 1).Select(ex => ex.Key).ToList(); // حق برداشت به تنهایی
+            var withdrawBySignersSign
+                = Customers.Where(ex => ex.Value == 3).Select(ex => ex.Key).ToList(); // حق برداشت در صورت کفایت امضا
+            var signerCustomers
+                = Customers.Where(ex => ex.Value == 2).Select(ex => ex.Key).ToList(); // حق امضا
+
+
+            if (mainCustomers.Count <= 0 && withdrawBySignersSign.Count <= 0)
+            {
+                MessageBox.Show("هیچ حالتی برای برداشت از حساب وجود ندارد");
+                return;
+            }
+
+            string strWithdrawMethods = string.Empty;
+            int methodsNum = 1;
+
+            if (mainCustomers.Count > 0)
+            {
+                strWithdrawMethods = $"حالت {methodsNum} : مشتریان {string.Join(",", mainCustomers)} امضا کرده باشند" + Environment.NewLine;
+                methodsNum += 1;
+            }
+            if (signerCustomers.Count > 0 && withdrawBySignersSign.Count > 0)
+            {
+                strWithdrawMethods += $"حالت {methodsNum} : { (withdrawBySignersSign.Count == 1 ? 2 : withdrawBySignersSign.Count) - 1} نفر از مشتریان  {string.Join(",", withdrawBySignersSign)} به همراه {(signerCustomers.Count == 1 ? 2 : signerCustomers.Count) - 1} نفر از مشتریان {string.Join(",", signerCustomers)}امضا کرده باشند" + Environment.NewLine;
+                methodsNum += 1;
+            }
+            if (withdrawBySignersSign.Count > 0)
+            {
+                strWithdrawMethods += $"حالت {methodsNum} : بیش از {(withdrawBySignersSign.Count == 1 ? 2 : withdrawBySignersSign.Count) - 1} نفر از مشتریان {string.Join(",", withdrawBySignersSign)} امضا کرده باشند";
+                methodsNum += 1;
+            }
+
+            MessageBox.Show(strWithdrawMethods, "حالت های برداشت", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+        }
+    }
+}
